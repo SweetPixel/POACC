d62ef0600aa1fffa8a3c10bf96a78f18977ea7ce
aliirz <dejavu87@gmail.com>
2013-07-08 21:24:15 +0500

    serving static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ POACC::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true  #fix this when depoying to your own server
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
