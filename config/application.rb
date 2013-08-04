5ff43e871ed7982455fa02443563fb868679b79d
aliirz <dejavu87@gmail.com>
2013-08-05 00:07:04 +0500

    icons not working in admin

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module POACC
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    
+    config.assets.enabled = true
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
