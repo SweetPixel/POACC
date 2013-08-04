107365e128c5065e09d2230ade11f1864d32730b
aliirz <dejavu87@gmail.com>
2013-08-04 22:40:32 +0500

    new relic now live

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,7 +3,7 @@ working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
-preload_app = true
+preload_app true
 listen "/tmp/unicorn.blog.sock"
 worker_processes 2
 timeout 30
