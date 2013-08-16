2ac9b1e3b74fad985953e82e5551a8ecad56644b
aliirz <dejavu87@gmail.com>
2013-08-16 17:57:33 +0500

    another attempt at fixing font awesome

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module POACC
     # -- all .rb files in that directory are automatically loaded.
 
     config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
