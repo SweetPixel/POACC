8cb9d3aadff6e5f912220156ab5851cf60e33229
aliirz <dejavu87@gmail.com>
2013-08-27 22:32:21 +0500

    added perform deliveries

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,7 @@ POACC::Application.configure do
 
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
