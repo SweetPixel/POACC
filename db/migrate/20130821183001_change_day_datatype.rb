3b23eb6c6faed7ddd2b66315aa9371299ec2611d
aliirz <dejavu87@gmail.com>
2013-08-28 13:02:18 +0500

    dropping appointments

--- db/migrate/20130821183001_change_day_datatype.rb
+++ db/migrate/20130821183001_change_day_datatype.rb
@@ -1,5 +1,13 @@
 class ChangeDayDatatype < ActiveRecord::Migration
-  def change
-  	change_column :appointments, :day, :date
+	def change
+  	# change_column :appointments, :day, :date
+  	drop_table :appointments
+  	create_table :appointments do |t|
+  		t.date :day
+  		t.time :requested_time
+  		t.integer :patient_id
+
+  		t.timestamps
+  	end
   end
 end
